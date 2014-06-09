a459c2fff9395b4536b1bb5e5644a8853fcf8e2f
omoman <mannysanchez998@gmail.com>
2014-06-09 14:43:59 -0700

    added scroll animation

--- assets/js/src/nav.js
+++ assets/js/src/nav.js
@@ -1,10 +1,27 @@
 /* All nav related functions */
 $(document).ready(function(){
 
-  $(window).scroll(function(){
-    if ($(window).scrollTop() > 100)
-      $('nav').addClass('nav-show');
+  var $offset_top = 100;
+  var $window = $(window);
+  var $nav = $('nav');
+  
+  /* Make navbar fade in after offset_top px and
+     fade back out before offset_top px */
+  $window.scroll(function(){
+    if ($window.scrollTop() > $offset_top)
+      $nav.addClass('nav-show');
     else
-      $('nav').removeClass('nav-show');
+      $nav.removeClass('nav-show');
   });
+
+  /* animation for href anchor to different segments
+     of page */
+  $nav.find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+  });
+  
 });
