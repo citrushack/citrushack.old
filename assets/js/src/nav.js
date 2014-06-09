d6f82135fb78d0a2de7212f42c6573a236e270fb
omoman <mannysanchez998@gmail.com>
2014-06-08 20:13:06 -0700

    added navbar transition function

--- assets/js/src/nav.js
+++ assets/js/src/nav.js
@@ -1,1 +1,10 @@
 /* All nav related functions */
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 100)
+      $('nav').addClass('nav-show');
+    else
+      $('nav').removeClass('nav-show');
+  });
+});
