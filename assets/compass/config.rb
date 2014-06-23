c97cd67c621e558d5df1163d5983a42e4fe160ab
omoman <mannysanchez998@gmail.com>
2014-06-22 17:38:51 -0700

    added fonts to build

--- assets/compass/config.rb
+++ assets/compass/config.rb
@@ -5,7 +5,7 @@ css_dir = "assets/css"
 sass_dir = "assets/compass/sass"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
-
+fonts_dir = "assets/fonts"
 
 output_style = :compressed
 relative_assets = true
